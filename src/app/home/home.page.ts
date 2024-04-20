3c45c1b05c42fa6cbf35e20af7fe42d38526a4db
Dhanush K <dhanushksgd@gmail.com>
2024-04-20 22:29:16 +0530

    fix: added skeleton loader task 4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 import { Asset } from '../shared/models/asset.model'
 import { AssetService } from '../shared/services/asset.service'
+import { getRandomWidth } from '../shared/functions';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +10,19 @@ import { AssetService } from '../shared/services/asset.service'
 })
 export class HomePage {
   assets: Asset[] = []
+  list: number[] = [1,2,3,4,5,6]
+  isLoading: boolean = false; 
+  skeletonWidths = [0, 1, 2].map(() => getRandomWidth(20, 70))
 
   constructor(private assetService: AssetService) {}
 
   ionViewWillEnter(): void {
     this.assets = []
+    this.isLoading = true;
     this.assetService.getAll().subscribe(response => {
       if (response.ok) {
         this.assets = response?.data;
+        this.isLoading = false; 
       } else {
         console.log('error')
       }
