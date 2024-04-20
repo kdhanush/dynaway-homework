33f94ce9b931a9d281c40bc360c2f0ecbae81746
Dhanush K <dhanushksgd@gmail.com>
2024-04-20 22:27:04 +0530

    fix : test case handling task 3

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -9,16 +9,35 @@ describe('HomePage', () => {
 
   beforeEach(waitForAsync(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents()
+      declarations: [HomePage],
+      imports: [IonicModule.forRoot()],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage)
-    component = fixture.componentInstance
-    fixture.detectChanges()
-  }))
+    fixture = TestBed.createComponent(HomePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
-  })
-})
+  it("should create the home page", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should have a title", () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector("ion-title").textContent).toEqual(
+      " Dynaway Homework "
+    );
+  });
+
+  it("should have a grid ", () => {
+    const compiled = fixture.nativeElement;
+    const grid = compiled.querySelector("ion-grid.list");
+    expect(grid).toBeTruthy();
+  });
+
+  it("should have app-asset-card", () => {
+    const compiled = fixture.nativeElement;
+    const assetCards = compiled.querySelectorAll("app-asset-card");
+    expect(assetCards.length).toBeGreaterThanOrEqual(0);
+  });
+});
